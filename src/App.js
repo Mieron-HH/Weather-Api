39df51f364ab4d379b5b4e9e0bc62a6574f301f1
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-23 02:53:57 +0400

    Weather Api - Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [weather, setWeather] = useState(null);
+	const [input, setInput] = useState("Abu Dhabi");
+	useEffect(() => {
+		axios
+			.get(
+				`http://api.weatherapi.com/v1/current.json?key=6b29b0d2ca094fbaad0213931222201&q=Abu Dhabi&aqi=yes`
+			)
+			.then((data) => {
+				setWeather(data.data);
+				console.log(data.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	const weatherInput = (e) => {
+		setInput(e.target.value);
+	};
+
+	const searchWeather = () => {
+		axios
+			.get(
+				`http://api.weatherapi.com/v1/current.json?key=6b29b0d2ca094fbaad0213931222201&q=${input}}&aqi=yes`
+			)
+			.then((data) => {
+				setWeather(data.data);
+				console.log(data.data);
+			})
+			.catch((err) => console.log(err));
+	};
+	return (
+		<div>
+			{weather && (
+				<div>
+					<div className="search">
+						<input type="text" onChange={weatherInput} />
+						<button onClick={searchWeather}>Search</button>
+					</div>
+
+					<div className="weather-info">
+						<h1>{weather.location.name}</h1>
+						<h2>{weather.location.country}</h2>
+						<div className="condition">
+							<h3>{weather.current.condition.text}</h3>
+							<img src={weather.current.condition.icon} alt="weather image" />
+							<h3>{weather.current.temp_c} degree Celsius</h3>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
